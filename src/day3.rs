256965599d1cd90f5ae7e56ddb642b864a20a429
Kaiqi Liang <kaiqi.liang@outlook.com>
2025-12-03 13:11:54 +0800

    Add input for day3

--- src/day3.rs
+++ src/day3.rs
@@ -2,11 +2,13 @@ use std::error::Error;
 
 use crate::Part;
 
-pub fn answer(input: &str, part: Part) -> Result<usize, Box<dyn Error>> {
+pub fn answer(input: &str, part: Part) -> Result<u8, Box<dyn Error>> {
     Ok(0)
 }
 
 #[cfg(test)]
 mod tests {
     use crate::{Part, day3::answer, example_challenge_2_parts_tests};
+
+    example_challenge_2_parts_tests!(3, 357, 0, 0, 0);
 }
